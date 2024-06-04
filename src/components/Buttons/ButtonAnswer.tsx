f8624f2af2d46d6636cb6a4f40a438f69cd67be7
Arseny <phone.kulikovarseniy@gmail.com>
2024-06-04 15:13:21 +0300

    new(2)

--- src/components/Buttons/ButtonAnswer.tsx
+++ src/components/Buttons/ButtonAnswer.tsx
@@ -1,10 +1,16 @@
 import { useDispatch } from 'react-redux';
 import { setNextNumber } from '../../store/slices/allDataSlice';
+import styles from './buttons.module.css';
 
 export default function ButtonAnswer() {
   const dispatch = useDispatch();
   const answerHandleClick = () => {
     dispatch(setNextNumber());
   };
-  return <button onClick={answerHandleClick}> Ответить </button>;
+  return (
+    <button className={styles.button} onClick={answerHandleClick}>
+      {' '}
+      Ответить{' '}
+    </button>
+  );
 }
